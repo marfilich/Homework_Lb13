5464154d3b7862e4c1388ece697022d5148cf02c
Мария Филичкина <marfilich@gmail.com>
2025-12-10 16:50:41 +0300

    Add files via upload

---
+++ homework_Lb13.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <ctype.h>
+#include <string.h>
+#define MAX 100
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char line[MAX];
+	char result_line[MAX];
+	int start = 0, i = 0;
+
+	puts("Введите строку:");
+	fgets(line, sizeof(line), stdin);
+	line[strcspn(line, "\n")] = '\0';
+	char* first = strchr(line, ' ');
+	char* last = strrchr(line, ' ');
+	strncpy(result_line, first + 1, last - first - 1);
+	result_line[last - first - 1] = '\0';
+	int k = strlen(result_line);
+	printf("Количество символов: %d", k);
+}
